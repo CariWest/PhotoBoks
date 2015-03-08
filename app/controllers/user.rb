1890da7b00b9e6acb85c758a3f9e4353c6230b8b
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-08 12:52:49 -0700

    user can log out

---
+++ app/controllers/user.rb
@@ -0,0 +1,29 @@
+get '/user/new' do
+  erb :"user/_new"
+end
+
+post '/user' do
+  user = User.create(params[:user])
+
+  if user.persisted?
+    session[:id] = user.id
+    redirect '/user'
+  else
+    erb :"user/_new", locals: { user: user }
+  end
+end
+
+get '/user' do
+  user = User.find(session[:id])
+  puts user
+  if user
+    puts "this is the user home page"
+    "hello world"
+    erb :"user/index", locals: { user: user}
+  end
+end
+
+get '/user/logout' do
+  session[:id] = nil
+  redirect '/'
+end
