4059502edd50c301859ff68713617dee2c0afa0c
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-08 12:40:55 -0700

    user can register

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,28 @@
 get '/' do
-  erb :index
+  user = User.new
+  erb :index, locals: { user: user }
 end
 
-get '/users/new' do
-  erb :"users/_new"
+get '/user/new' do
+  erb :"user/_new"
+end
+
+post '/user' do
+  user = User.create(params[:user])
+
+  if user.persisted?
+    session[:id] = user.id
+    redirect '/user'
+  else
+    erb :"user/_new", locals: { user: user }
+  end
+end
+
+get '/user' do
+  user = User.find(session[:id])
+  puts user
+  if user
+    puts "this is the user home page"
+    "hello world"
+  end
 end
