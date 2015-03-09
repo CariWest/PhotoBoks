3ca154788b076c8797c7d5a4986eb4e930bebafe
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-08 23:08:50 -0700

    troubleshooting javascript side of albums

--- public/js/application.js
+++ public/js/application.js
@@ -8,45 +8,25 @@ var Album = function(title, tag, cover) {
   this.title = title;
   this.tag = tag;
   this.cover = cover;
-  // removed album tag for now... should add that in asap
 }
 
-Album.prototype.buildAlbumElement = function(albumName, albumCover) {
-  // removed albumTag from this for now...
+Album.prototype.buildAlbumElement = function(albumName, albumTag, albumCover) {
   var albumTemplate = $.trim($('#album_template').html());
   var $album = $(albumTemplate);
   $album.find('img').attr('src', albumCover);
-  // $album.find('.tag').html(albumTag)
+  $album.find('.tag').html(albumTag)
   $album.find('h2').text(albumName);
   return $album;
 }
 
-Album.View = function() {
+// Album.View = function() {
 
-}
+// }
 
-$(document).ready(function() {
-
-  $('.new-album').on('click', function(event) {
-    event.preventDefault();
-
-    var request = $.ajax({
-      url: NEW_ALBUM_URL,
-      type: 'get'
-    });
-
-    request.done( function(data) {
-      $(data.form).insertAfter('.welcome'); // should abstract this away.
-    });
-
-    request.fail( function(data) {
-      console.log("new album form fails to appear")
-    });
-  });
-});
 
 AlbumCollection = function(formSelector) {
-  this.view = new AlbumCollection.view(formSelector);
+  debugger
+  this.view = new AlbumCollection.View(formSelector);
   this.isListening = false;
   this.listenForNewAlbums();
   this.albums = [];
@@ -61,12 +41,12 @@ AlbumCollection.prototype.listenForNewAlbums = function() {
 AlbumCollection.prototype.makeNewAlbum = function(event) {
   event.preventDefault();
   var album = Album.create(
-    this.view.$elt.find('.title');
-    this.view.$elt.find('.tag');
-    this.view.$elt.find('.cover');
+    this.view.$elt.find('.title'),
+    this.view.$elt.find('.tag'),
+    this.view.$elt.find('.cover')
   );
 
-  this.albums.push(album);
+  this.albums.push(album); // still need to do something with the album we've created after this to add it to the page
 }
 
 AlbumCollection.View = function(formSelector) {
@@ -80,3 +60,26 @@ AlbumCollection.View.prototype = createNewAlbumListener = function(callback) {
 AlbumCollection.View.getMakeAlbumButton = function() {
   return this.$elt.find('#create-album')
 }
+
+// controller
+$(document).ready(function() {
+
+  $('.new-album').on('click', function(event) {
+    event.preventDefault();
+
+    var request = $.ajax({
+      url: NEW_ALBUM_URL,
+      type: 'get'
+    });
+
+    request.done( function(data) {
+      $(data.form).insertAfter('.welcome'); // should abstract this jquery away...
+      var controller = new AlbumCollection('#new-album-form');
+    });
+
+    request.fail( function(data) {
+      console.log("new album form fails to appear");
+    });
+  });
+
+});
