78a652d2d4164c06452e6068ded2f056235b7b68
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-12 15:35:16 -0700

    experimenting with javascript

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+
+
+var getSingleAlbum = function(targetAlbum) {
+
+}
+
+$(document).ready(function() {
+
+  $('.content').on('click', '.picture', function(){
+    var pictures = getAlbumPictures();
+    // get rid of current content
+    // get the album's pictures
+    // render the album partial repeatedly on that page
+  });
+
+})
