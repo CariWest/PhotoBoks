3ed6348e25b8efe9cb11088921aff4cfd8667c48
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-11 19:31:52 -0700

    format hashtag properly for user

--- app/helpers/general_helpers.rb
+++ app/helpers/general_helpers.rb
@@ -5,6 +5,7 @@ helpers do
 
   # this method is so ugly. Refactor me eventually!
   def find_or_create_tag(tag_name)
+    tag_name.gsub!("#", "")
     if Tag.find_by(name: tag_name)
       @tag = Tag.find_by(name: tag_name)
     else
